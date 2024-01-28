809351e52b23bbe7833fc2180564c34074775b7c
IgorDv5 <igoraluno2005@gmail.com>
2024-01-28 19:07:32 -0300

    Metodo Put Para Tecnico implementado

--- src/main/java/com/igor/helpdesk/domain/Pessoa.java
+++ src/main/java/com/igor/helpdesk/domain/Pessoa.java
@@ -16,6 +16,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.igor.helpdesk.domain.enums.Perfil;
 
@@ -28,6 +30,7 @@ public abstract class Pessoa implements Serializable {
 	protected Integer id;
 	protected String nome;
 
+	@CPF
 	@Column(unique = true) // Anotacao Para Informar que o Atributo é Unico
 	protected String cpf;
 	@Column(unique = true) // Anotacao Para Informar que o Atributo é Unico
