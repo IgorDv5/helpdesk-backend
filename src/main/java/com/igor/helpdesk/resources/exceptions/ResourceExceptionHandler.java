98afb7f188279242c68860425f073756350b79cd
IgorDv5 <igoraluno2005@gmail.com>
2024-01-28 16:24:01 -0300

    Exceção para Objeto Não Encontrado

---
+++ src/main/java/com/igor/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.igor.helpdesk.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.igor.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	// Manipula a exceção ObjectNotFoundException e retorna uma resposta personalizada
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
+			HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				                            "Object Not Found", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+	
+}
